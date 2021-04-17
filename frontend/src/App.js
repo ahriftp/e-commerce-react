3848c84548b82d208cc770f5a855d3625772a476
ahriftp <ahriftp@gmail.com>
2021-04-17 17:06:54 +0800

    Frontend Progress
    built:
    ProductScreen.js - A screen to display product page
    
    Installed React Router and Router Bootstrap
    Replaced <a href> into <Link to>, webpage does not reload anymore
    Header component uses Router Bootstrap to wrap Navbar components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,28 @@
+// Packages
 import React from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import {Container} from 'react-bootstrap'
+
+// Components
 import Header from './components/Header'
 import Footer from './components/Footer'
+
+// Screens
 import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main className = 'py-3'>
         <Container>
-          <HomeScreen />
+          <Route path = '/' component = {HomeScreen} exact/>
+          <Route path = '/product/:id' component = {ProductScreen}/>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
