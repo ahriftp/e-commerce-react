983ae19397c76ce04a4dcf523d96cebdfc0fa2df
ahriftp <ahriftp@gmail.com>
2021-04-18 21:42:58 +0800

    Backend Progress
    Installed async-handler
    
    routes.js created. Used to get product information from database. Less clutter in server.js
    Route retrieves data from mongoDB instead of local dummy data

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import asyncHandler from 'express-async-handler'
+import Product from '../models/productModel.js'
+
+const router = express.Router()
+
+// Get all products using mongoose model
+// @route   GET /api/products
+// @access  Public
+router.get('/', asyncHandler(async (req, res) => {
+    const products = await Product.find({})
+
+    res.json(products)
+}))
+
+// Get specific product by ID using link to get ID
+// @route   GET /api/products/:id
+// @access  Public
+router.get('/:id', asyncHandler(async(req, res) => {
+    const product = await Product.findById(req.params.id)
+
+    if(product){
+        res.json(product)
+    } else {
+        res.status(404).json({message : "Product not found."})
+    }
+}))
+
+export default router
