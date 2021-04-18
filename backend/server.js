6a95b62c7eaf1e805dd38f10a58408b1bad8794f
ahriftp <ahriftp@gmail.com>
2021-04-18 21:54:48 +0800

    Backend Middleware Errorhandler
    Added errorMiddleware.js to handle errors for route not found and product not found.

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import colors from 'colors'
 // Modules
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 
 dotenv.config()
 
@@ -21,5 +22,9 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
+
+// Middleware error handler
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(PORT, console.log(`Server running in ${ENV} mode on port ${PORT}`.green.bold))
