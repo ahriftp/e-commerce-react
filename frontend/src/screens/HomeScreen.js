c08bf94b93f6b9708702d282d23d33e4facd7887
ahriftp <ahriftp@gmail.com>
2021-04-20 01:55:47 +0800

    Frontend Changed screen to use Redux
    Made changes to HomeScreen.js to use previously built redux components to retrieve product listing

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,28 +1,29 @@
 // Packages
-import axios from 'axios'
 import React, { useState, useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+
+// Redux Components
+import { listProducts } from '../actions/productActions'
 
 // Components
 import Product from '../components/Product'
 
 const HomeScreen = () => {
-    // useState and useEffect used here because this is not a class
-    const [products, setProducts] = useState([])
+    const dispatch = useDispatch()
+    
+    const productList = useSelector((state) => state.productList)
+    const { loading, error, products } = productList
 
     useEffect(() => {
-        const fetchProducts = async () => {
-            const { data } = await axios.get('/api/products')
-            
-            setProducts(data)
-        }
-
-        fetchProducts()
-    }, [])
+        dispatch(listProducts())
+    }, [dispatch])
 
     return (
         <>
             <h1>Latest Products</h1>
+            {loading ? <h2>Loading...</h2> : 
+            error ? <h2>{error}</h2> :
             <Row>
                 {products.map(product => (
                     <Col key = {product._id} sm = {12} md = {6} lg = {4} xl = {3}>
@@ -30,6 +31,8 @@ const HomeScreen = () => {
                     </Col>
                 ))}
             </Row>
+            }
+            
         </>
     )
 }
