0f9782ebda44c1042826cc0e20d4fdfff0539560
ahriftp <ahriftp@gmail.com>
2021-04-20 20:27:25 +0800

    Backend User Auth Setup
    Created user controller and route.
    Added function to schema file userModel.js.
    Added middleware express.json() to use req.body

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,12 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 
+// Routes
+import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+
 // Modules
 import connectDB from './config/db.js'
-import productRoutes from './routes/productRoutes.js'
 import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 
 dotenv.config()
@@ -21,10 +24,16 @@ app.get('/', (req, res) => {
     res.send('API is running')
 })
 
+// Middleware body parser
+app.use(express.json())
+
+// Routes
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 // Middleware error handler
 app.use(notFound)
 app.use(errorHandler)
+
 
 app.listen(PORT, console.log(`Server running in ${ENV} mode on port ${PORT}`.green.bold))
